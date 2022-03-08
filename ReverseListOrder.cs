cadeb15c9eaf7df9f77c9574017af81041090c31
Alexsander Faria <alexsander.itj@hotmail.com>
2022-03-07 23:33:08 -0300

    comentarios removidos

--- ReverseListOrder.cs
+++ ReverseListOrder.cs
@@ -10,8 +10,6 @@ namespace ExerciciosCodeWars
     {
         public static List<int> ReverseList(List<int> list)
         {
-            // Return a new list with its elements in reverse order compared to the input list
-            // Do not mutate the original list!
             List<int> reverseList = new List<int>();
             foreach (var item in list)
             {
