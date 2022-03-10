d51021d6976963c670315e8ec5d064d4abb932c0
Alexsander Faria <alexsander.itj@hotmail.com>
2022-03-10 20:42:30 -0300

    kata done!

---
+++ Defineacardsuit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosCodeWars
+{
+    internal class Defineacardsuit
+    {
+        public static string DefineSuit(string card)
+        {
+
+            if (card.Contains("♣"))
+                return "clubs";
+            
+            if (card.Contains("♦"))
+                return "diamonds";
+            
+            if (card.Contains("♥"))
+                return "hearts";
+            
+            if (card.Contains("♠"))
+                return "spades";
+            
+
+            return "";
+        }
+    }
+}
