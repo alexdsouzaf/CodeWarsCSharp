34f3f8d82cba426f5797de24f554e23499ea78d8
Alexsander Faria <alexsander.itj@hotmail.com>
2022-03-10 21:38:31 -0300

    kata done

---
+++ IsItEven.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosCodeWars
+{
+    internal class IsItEven
+    {
+        /// <summary>
+        /// verificar se o numero é par (ou nao)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public bool IsEven(double n) => (n % 2 == 0);
+
+    }
+}
