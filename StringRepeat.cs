bad03334c1b0eb2c32ad31b8401d87b19a2e7bf9
Alexsander Faria <alexsander.itj@hotmail.com>
2022-03-14 23:48:28 -0300

    kata done

---
+++ StringRepeat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosCodeWars
+{
+    internal class StringRepeat
+    {
+        public static string RepeatStr(int n, string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                sb.Append(s);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
