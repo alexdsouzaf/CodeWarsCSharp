0cd5a20daba98767da7407e9216ae604b2871a5d
Alexsander Faria <alexsander.itj@hotmail.com>
2022-04-23 18:55:02 -0300

    Kata resolvido

---
+++ CatsAndDogsYear.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosCodeWars
+{
+    internal class CatsAndDogsYear
+    {
+        public static int[] humanYearsCatYearsDogYears(int humanYears) => new int[] { humanYears, Years(humanYears,true), Years(humanYears, false) };
+
+
+
+        private static int Years(int humanYears, bool IsCat) //or IsDog whatever
+        {
+            int year = 0;
+            if (humanYears == 1)
+                return year = 15;
+
+            if (humanYears == 2)
+                return year = 15 + 9;
+
+            if (humanYears > 2)
+            {
+                year = 15 + 9;
+                for (int i = 3; i <= humanYears; i++)
+                {
+                    if (IsCat)
+                        year += 4;
+                    else
+                        year += 5;
+                }
+            }
+
+            return year;
+        }
+    }
+}
