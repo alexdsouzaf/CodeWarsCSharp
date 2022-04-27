d9002ad8252e6b31c22261a36ecc23a36c1341df
Alexsander Faria <alexsander.itj@hotmail.com>
2022-04-26 21:02:12 -0300

    Kata resolvido

--- CheckSameCase.cs
+++ CheckSameCase.cs
@@ -8,6 +8,37 @@ namespace ExerciciosCodeWars
 {
     internal class CheckSameCase
     {
+        #region[First Solution]
+        static string upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        static string lowerCase = "abcdefghijklmnopqrstuvwxyz";
+        public static int SameCase(char a, char b)
+        {
+            //same case return 1
+            if ( ( IsUpper(a) && IsUpper(b) ) || ( IsLower(a) && IsLower(b) ) )
+                return 1;
 
+
+            //diferent case return 0
+            if (( IsUpper(a) && IsLower(b) ) || IsUpper(b) && IsLower(a))
+                return 0;
+            
+
+            //not letter -1
+            return -1;
+        }
+
+        private static bool IsUpper(char p) => upperCase.Contains(p);
+        private static  bool IsLower(char p) => lowerCase.Contains(p);
+        #endregion
+
+        #region[Second Solution]
+        public static int SameCase2(char a, char b)
+        {
+            if (!char.IsLetter(a) || !char.IsLetter(b))
+                return -1;
+
+            return char.IsLower(a) == char.IsLower(b) ? 1 : 0;
+        }
+        #endregion
     }
 }
