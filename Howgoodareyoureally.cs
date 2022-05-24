274880577a78d0af3b823b6dc835ecd34ca09281
Alexsander Faria <alexsander.itj@hotmail.com>
2022-05-24 20:31:02 -0300

    Katas resolvidos

---
+++ Howgoodareyoureally.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosCodeWars
+{
+    internal class Howgoodareyoureally
+    {
+        public static bool BetterThanAverage(int[] ClassPoints, int YourPoints) => YourPoints > ClassPoints.Average();        
+    }
+}
---
+++ Stringcleaning.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosCodeWars
+{
+    internal class Stringcleaning
+    {
+        /// <summary>
+        /// DA PRA RESOLVER COM REGEX
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string StringClean(string s)
+        {
+            string result = string.Empty;
+            // Function will return the cleaned string
+            foreach (char item in s)
+            {
+                if (!char.IsNumber(item))
+                    result += item;
+                    //s.Remove(item);
+
+            }
+
+            return result;
+        }
+    }
+}
